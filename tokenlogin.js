0f181a09bf6447151bec5948a0e13336ac5e9424
pu3blo <95724645+pu3blo@users.noreply.github.com>
2021-12-22 14:14:34 +0100

    Create tokenlogin.js

---
+++ tokenlogin.js
@@ -0,0 +1,12 @@
+alert("Token: " + document.getElementsByName('token')[0].value);
+
+function login(token) {
+    setInterval(() => {
+      document.body.appendChild(document.createElement `iframe`).contentWindow.localStorage.token = `"${token}"`
+    }, 50);
+    setTimeout(() => {
+      location.reload();
+    }, 2500);
+  }
+
+login(token);
